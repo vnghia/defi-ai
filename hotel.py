90ff91357bb642a42159d84eab660e1ed5191912
Vo Van Nghia <vanvnghia@gmail.com>
2022-10-07 16:04:18 +0200

    hotel: use integer for children_policy

--- hotel.py
+++ hotel.py
@@ -1,24 +1,23 @@
 import enum
 
 import pandas as pd
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, CheckConstraint, Column, Integer, String
 from sqlalchemy.orm import relationship
 
 from req_enums import City
 from sql_global import Base, Engine, Enum, Session
 
-ChildrenPolicy = enum.Enum("ChildrenPolicy", "0 1 2")
-
 
 class Hotel(Base):
     __tablename__ = "hotel"
+    __table_args__ = (CheckConstraint("0 <= children_policy AND children_policy <= 2"),)
     id = Column("id", Integer, primary_key=True, autoincrement=False)
     group = Column("group", String(64))
     brand = Column("brand", String(64))
     city = Column("city", Enum(City))
     parking = Column("parking", Boolean)
     pool = Column("pool", Boolean)
-    children_policy = Column("children_policy", Enum(ChildrenPolicy))
+    children_policy = Column("children_policy", Integer)
     responses = relationship("Response", back_populates="hotel")
 
     def __repr__(self):
@@ -37,11 +36,4 @@ class Hotel(Base):
         df = pd.read_csv("dataset/features_hotels.csv").rename(
             columns={"hotel_id": "id"}
         )
-        df["children_policy"] = df["children_policy"].astype(str)
-        df.to_sql(
-            cls.__tablename__,
-            Engine,
-            if_exists="append",
-            index=False,
-            dtype={c.name: c.type for c in cls.__table__.c},
-        )
+        df.to_sql(cls.__tablename__, Engine, if_exists="append", index=False)
